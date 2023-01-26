1c2ab24986438f6fe5aaa180f16a92d733c9c776
DeluxeNation <Dragonman0901@gmail.com>
2023-01-26 14:15:06 -0600

    Renamed timer class to support more timers.

--- DeluxiaScripts-CSharp/DeluxiaTimers.cs
+++ DeluxiaScripts-CSharp/DeluxiaTimers.cs
@@ -5,28 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Deluxia{
-	public class DeluxiaIntTimer {
+	public class DeluxiaSecondTimer {
 		public int timeLeft { get; private set; }
+		private int startingTime;
 		public bool pause;
 		private Action action;
-		public DeluxiaIntTimer(int timer,Action activate,bool start) {
+		public DeluxiaSecondTimer(int timer,Action activate,bool start) {
 			pause = false;
 			timeLeft = timer;
+			startingTime = timeLeft;
 			action = activate;
 			if(start) {
 				_ = Start();
 			}
 			
 		}
+		public async Task Restart() {
+			timeLeft = startingTime;
+			await Start();
+		}
 		public async Task Start() {
+			UnityEngine.Debug.LogWarning("Start");
+			pause = false;
 			while(timeLeft > 0 && !pause) {
 				timeLeft--;
 				await Task.Delay(1000);
 			}
+			UnityEngine.Debug.LogWarning("End Timer");
 			if(!pause) {
 				action();
 			}
 
+		}
+		public void Stop() {
+			UnityEngine.Debug.LogWarning("Stop");
+			pause = true;
+			timeLeft = startingTime;
 		}
 	}
 }
