faef093f98e8685cc4febe545b028be19797f1ca
DeluxeNation <Dragonman0901@gmail.com>
2023-01-26 01:07:21 -0600

    Random now supports IEnumerables. New Timer Class.

---
+++ DeluxiaScripts-CSharp/DeluxiaIntTimer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deluxia{
+	public class DeluxiaIntTimer {
+		public int timeLeft { get; private set; }
+		public bool pause;
+		private Action action;
+		public DeluxiaIntTimer(int timer,Action activate,bool start) {
+			pause = false;
+			timeLeft = timer;
+			action = activate;
+			if(start) {
+				_ = Start();
+			}
+			
+		}
+		public async Task Start() {
+			while(timeLeft > 0 && !pause) {
+				timeLeft--;
+				await Task.Delay(1000);
+			}
+			if(!pause) {
+				action();
+			}
+
+		}
+	}
+}
